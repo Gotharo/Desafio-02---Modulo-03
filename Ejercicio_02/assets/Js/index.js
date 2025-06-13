dab1200c6da6edb03b8a64608913b976ce109a2f
Gonzalo Castro <gonzalo.castro@gotharo.cl>
2025-06-13 14:07:10 -0400

    feature: se finaliza el 2do ejercicio

--- Ejercicio_02/assets/Js/index.js
+++ Ejercicio_02/assets/Js/index.js
@@ -1,3 +1,24 @@
+const btnVerificar = document.querySelector("#btn-check");
+const textWarning = document.querySelector("#text-warning");
+const a = Number(document.querySelector("#count1").value);
+const b = Number(document.querySelector("#count2").value);
+const c = Number(document.querySelector("#count3").value);
+let suma = a + b + c;
 
+btnVerificar.addEventListener("click", allStickers)
 
-// console.log(img)
+function allStickers () {
+    const a = Number(document.getElementById('count1').value);
+    const b = Number(document.getElementById('count2').value);
+    const c = Number(document.getElementById('count3').value);
+    const suma = a + b + c;
+    console.log(suma);
+    if(suma >= 10){
+            textWarning.textContent = "llevas demaciados Stickers"
+        } else {
+                return textWarning.textContent = `llevas  ${suma}  stickers` ;
+            }
+            
+};
+        
+
